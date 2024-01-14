d37b5163fa0822ff3a763ebf6d8f0279e3116141
SujinHam <130124538+SujinHam@users.noreply.github.com>
2024-01-14 23:26:43 +0900

    [level Lv.0] Title: 문자열 여러 번 뒤집기, Time: 2.15 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -0,0 +1,6 @@
+def solution(my_string, queries):
+    answer = list(my_string)
+    for start, end in queries : 
+        answer[start:end + 1] = answer[start:end+1][::-1]
+    answer = ''.join(answer)    
+    return answer
